f1e4cc75e042118a33c0b02599c95335eae54ef7
Artem <254648939+Ordboybro@users.noreply.github.com>
2026-08-18 12:51:25 +0700

    Connect functional file actions to desktop window

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QApplication
 
 from config import APP_NAME, THEME
 from ui.main_window import MainWindow
+from modules.ui_actions import UIActions
 
 
 def main() -> int:
@@ -11,10 +12,12 @@ def main() -> int:
     app.setApplicationName(APP_NAME)
 
     if THEME.exists():
-        THEME_CONTENT = THEME.read_text(encoding="utf-8")
-        app.setStyleSheet(THEME_CONTENT)
+        theme = THEME.read_text(encoding="utf-8")
+        app.setStyleSheet(theme)
 
     window = MainWindow()
+    actions = UIActions(window)
+    actions.connect()
     window.show()
 
     return app.exec()
